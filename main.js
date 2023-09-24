be1bdfe60a25e5321181da1e348dad2b37fdae62
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2023-09-24 21:21:26 +0300

    Solved the 8th task

--- main.js
+++ main.js
@@ -234,3 +234,29 @@
 //
 // friend(["Ryan", "Kieran", "Mark"])
 
+
+// 8
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+//
+//     Note: a and b are not ordered!
+//
+//     Examples (a, b) --> output (explanation)
+// function getSum(a, b) {
+//     if (a===b){
+//         return a
+//     }else {
+//         let arr = [a,b].sort((a,b)=>a-b)
+//         let result = 0
+//         for (let i = arr[0]; i <= arr[1]; i++) {
+//             result += i
+//         }
+//         return result
+//     }
+// }
+//
+// getSum(-1, 2)
+
+
+
+
