0279a4c653995acb5f886f1bd28fe11e3a8ecf05
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2023-09-26 20:42:56 +0300

    Solved the 14th task

--- main.js
+++ main.js
@@ -415,3 +415,29 @@
 // }
 //
 // solution(10)
+
+
+// 14
+
+// Complete the solution so that the function will break up camel casing, using a space between words.
+//
+//     Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+// complete the function
+// function solution(string) {
+//     let result = '';
+//
+//     for (let i = 0; i < string.length; i++) {
+//         if (string[i] === string[i].toLowerCase()) {
+//             result += string[i]
+//         } else {
+//             result += ` ${string[i]}`
+//         }
+//     }
+//     return result
+// }
+//
+// solution('camelCasing')
+// solution('identifier')
