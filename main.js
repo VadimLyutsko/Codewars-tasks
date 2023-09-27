6731692ce0666af9626993acbf6c6215878776e2
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2023-09-27 22:09:13 +0300

    Solved the 16th task

--- main.js
+++ main.js
@@ -476,3 +476,21 @@
 // generateHashtag('Hello there thanks for trying my Kata')
 // generateHashtag("    Hello     World   ")
 // generateHashtag("Do We have A Hashtag")
+
+
+// 16
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+//
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+//
+// function moveZeros(arr) {
+//     let resultArr = [];
+//     let zerosArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         arr[i] !== 0 ? resultArr.push(arr[i]) : zerosArr.push(0)
+//     }
+//     return [...resultArr, ...zerosArr]
+// }
+//
+// moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"])
