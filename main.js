551c4b53913c8dc97261d7c625ab34e4c8a01dfb
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2023-09-28 20:26:54 +0300

    Solved the 17th task part-2

--- main.js
+++ main.js
@@ -494,3 +494,63 @@
 // }
 //
 // moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"])
+
+
+// 17
+
+// Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+//
+//     Example:
+//
+// Given an input string of:
+//
+//     apples, pears # and bananas
+// grapes
+// bananas !apples
+// The output expected would be:
+//
+//     apples, pears
+// grapes
+// bananas
+// The code would be called like so:
+//
+//     var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+// // result should == "apples, pears\ngrapes\nbananas"
+
+// function solution(input, markers) {
+//     input = input.split('\n')
+//     let res = []
+//
+//     for (let i = 0; i < input.length; i++) {
+//         // console.log(input[i].indexOf('#'))
+//         if (input[i].includes('#')) {
+//             res.push(input[i].slice(0, input[i].indexOf('#')) + '\n')
+//             // console.log(input[i].slice(0, input[i].indexOf('#')))
+//         } else if (!input[i].includes('#') && !input[i].includes('!')) {
+//             res.push(input[i] + '\n')
+//             // console.log(input[i])
+//         }
+//         else if (input[i].includes('!')) {
+//             res.push(input[i].slice(0, input[i].indexOf('!')))
+//             // console.log(input[i].slice(0, input[i].indexOf('!')) )
+//         }
+//         // console.log(input[i].length)
+//         // for (let j = 0; j < input[i].length; j++) {
+//         //     console.log(input[i][j])
+//         // }
+//
+//     }
+//
+//
+//     console.log(res.join('').trim())
+//     return res.join('').trim()
+// // }
+//
+// function solution(input, markers) {
+//     return  input
+//         .split('\n')
+//         .map((s) => markers.reduce((t, m) => t.split(m)[0].trim(), s))
+//         .join('\n');
+// }
+//
+// solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
