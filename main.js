7c183b164fdcbb35256a3a7a3988d6c5d7e90bcf
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2023-10-03 18:21:02 +0300

    Solved the 19th task

--- main.js
+++ main.js
@@ -617,3 +617,48 @@
 //     if (isSorted) break;
 // }
 // console.log(nums)
+
+
+// 19
+
+
+// In this kata we want to convert a string into an integer. The strings simply represent the numbers in words.
+//
+//     Examples:
+//
+// "one" => 1
+// "twenty" => 20
+// "two hundred forty-six" => 246
+// "seven hundred eighty-three thousand nine hundred and nineteen" => 783919
+// Additional Notes:
+//
+//     The minimum number is "zero" (inclusively)
+// The maximum number, which must be supported is 1 million (inclusively)
+// The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
+// All tested numbers are valid, you don't need to validate them
+//
+// function parseInt(string){
+//     var ref = { one:1, two:2, three:3, four:4, five:5, six:6, seven:7, eight:8, nine:9, ten:10, eleven:11, twelve:12, thirteen:13, fourteen:14, fifteen:15, sixteen:16, seventeen:17, eighteen:18, nineteen:19, twenty:20, thirty: 30, forty: 40, fifty: 50, sixty: 60, seventy: 70, eighty: 80, ninety:90 },
+//         mult = { hundred: 100, thousand: 1000, million: 1000000 },
+//         strNums = string.split(' ').reverse(),
+//         number = 0,
+//         multiplier = 1;
+//
+//     for(i in strNums){
+//         if( mult[strNums[i]] != undefined ) {
+//             if(mult[strNums[i]]==100) {
+//                 multiplier*=mult[strNums[i]]
+//             }else{
+//                 multiplier=mult[strNums[i]]
+//             }
+//         } else {
+//             if (!isNaN(parseFloat(strNums[i]))) {
+//                 number += parseFloat(strNums[i]) * multiplier;
+//             } else {
+//                 var nums = strNums[i].split('-');
+//                 number += ((ref[nums[0]]||0) + (ref[nums[1]]||0)) * multiplier;
+//             }
+//         }
+//     }
+//     return number;
+// }
