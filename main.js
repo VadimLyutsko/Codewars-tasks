e8d84cea393d5b00f3ac87c5683b6a2f9cd42153
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2023-10-21 11:05:12 +0300

    Хук useDebounce

--- main.js
+++ main.js
@@ -663,3 +663,36 @@
 //     return number;
 // }
 //
+// Хук useDebounce
+//
+// const useDebounce = (value, delay) => {
+//     const [debouncedValue, setDebouncedValue] = useState(value);
+//
+//     useEffect(() => {
+//         const timeout = setTimeout(() => {
+//             setDebouncedValue(value);
+//         }, delay);
+//
+//         return () => {
+//             clearTimeout(timeout);
+//         };
+//     }, [value]);
+//
+//     return debouncedValue;
+// };
+//
+// // Использование
+//
+// const Counter = () => {
+//     const [value, setValue] = useState(0);
+//     const lastValue = useDebounce(value, 1000);
+//
+//     return (
+//         <div>
+//             <p>
+//                 Current Value: {value} | Debounced Value: {lastValue}
+//             </p>
+//             <button onClick={() => setValue(value + 1)}>Increment</button>
+//         </div>
+//     );
+// };
