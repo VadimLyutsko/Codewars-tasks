c2439ae9c27412c238cf4e9eec8cc56ac3a4e82c
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2023-11-08 20:34:14 +0300

    Solved the 20th task

--- main.js
+++ main.js
@@ -698,7 +698,6 @@
 // };
 
 
-
 // Started studying BEM - methodology
 // const f = (s) => {
 //     let arr = []
@@ -717,3 +716,18 @@
 // }
 //
 // f('Started studying BEM METHODOLOGY')
+
+// 20
+
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+// function solution(str){
+//     var arr = str.split('')
+//     var res = []
+//
+//     for(let i = 0; i<arr.length; i+=2){
+//         if(arr[i+1]) res.push(arr[i]+arr[i+1])
+//         else res.push(arr[i]+'_')
+//     }
+//
+//     return res
+// }
