25a5f1f1256baad9346a1e50972fbeea408aeffb
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2024-03-12 15:23:34 +0300

    Added the 25'th task

--- main.js
+++ main.js
@@ -852,3 +852,24 @@
 // console.log(duplicateCount("Indivisibilities"))
 // console.log(duplicateCount("aA11"))
 // console.log(duplicateCount("ABBA"))
+
+//25
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+//
+//     Your task is to write a function which returns the time since midnight in milliseconds.
+//
+//     Example:
+// h = 0
+// m = 1
+// s = 1
+//
+// result = 61000
+//
+// function past(h, m, s){
+//     var miliseconds = 0;
+//     miliseconds = miliseconds + s * 1000;
+//     miliseconds = miliseconds + m * 60000;
+//     miliseconds = miliseconds + h * 3600000;
+//     return miliseconds;
+// }
